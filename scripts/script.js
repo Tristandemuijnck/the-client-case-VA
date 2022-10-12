ebf791989dda24ceb3567925988b27e3537bfa10
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2022-10-12 13:13:37 +0200

    Open/Close all buttons

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,31 @@ function switchDisplay(id, buttonId){
         button.innerHTML = "Meer info";
     }
 }
+
+function switchAllDisplay(){
+    const classDisplay = document.querySelectorAll('.article-extra');
+
+    classDisplay.forEach((classDisplayChild) => {
+        if(classDisplayChild.style.display == 'none'){
+            classDisplayChild.style.setProperty('display', 'flex');
+            switchButtonText(classDisplayChild.style.display);
+        }
+        else if(classDisplayChild.style.display == 'flex'){
+            classDisplayChild.style.setProperty('display', 'none');
+            switchButtonText(classDisplayChild.style.display);
+        }
+    });
+}
+
+function switchButtonText(classDisplayChild){
+    const infoButton = document.querySelectorAll('.info-btn');
+
+    infoButton.forEach((infoButtonChild) => {
+        if(classDisplayChild == 'none'){
+            infoButtonChild.innerHTML = "Meer info";
+        }
+        else if(classDisplayChild == 'flex'){
+            infoButtonChild.innerHTML = "Minder info";
+        }
+    });
+}
